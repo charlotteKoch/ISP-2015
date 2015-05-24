1406cc4a0c7311943cea4570e2045ac2274fd73f
Adrienne7 <aklein@bayviewglen.ca>
2015-05-23 23:32:01 -0400

    change of character

--- src/com/bayviewglen/zork/Character.java
+++ src/com/bayviewglen/zork/Character.java
@@ -2,5 +2,11 @@ package com.bayviewglen.zork;
 
 public class Character {
 	private String name;
+	private Room location;
+
+	public Character(String characterName, Room characterLocation) {
+		name = characterName;
+		location = characterLocation;
+	}
 
 }
